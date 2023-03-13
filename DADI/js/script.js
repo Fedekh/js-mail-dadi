208c9d8be6690815b0ba0eb77a54f888f4c3f3cb
Fedekh <federicocet@gmail.com>
2023-03-13 18:19:15 +0100

    add all decoration and milestone 2 of dice game

--- DADI/js/script.js
+++ DADI/js/script.js
@@ -34,11 +34,63 @@
 
 // MILESTONE 2
 
+// ho voluto provare a usare piu manipolazione js per creare elementi e aggiungere o togliere classi
+
 const wrapper = document.querySelector (".wrapper");
+const submit = document.querySelector(".submit");
+const userWin = document.querySelector(".hidden");
+const pcWin = document.querySelector(".hidden:nth-of-type(2)");
+const parity = document.querySelector(".hidden:nth-of-type(3)");
+
+
 const title = document.createElement("h1");
-title.innerHTML = "GIOCA A DADI";
+const subtitle = document.createElement("h2");
+const subtitle2 = document.createElement("h3");
+
+title.innerHTML = "PROVA ANCHE TU A SFIDARE LA FORTUNA";
+subtitle.innerHTML = "Clicca nel bottone in basso per generare un numero random";
+subtitle2.innerHTML = "Il pc ne genenererà un altro per se, vince il maggiore, DAJE";
+
+title.classList.add ("py-2");
+subtitle.classList.add ("py-4");
+subtitle2.classList.add ("py-4");
+
+
+wrapper.prepend (subtitle2);
+wrapper.prepend (subtitle);
 wrapper.prepend (title);
+let user = document.querySelector(".user-random-number");
+let pcN = document.querySelector(".pc-random-number");
 
-let user = document.querySelector ("user-random-number");
-let pcN = document.querySelector (".pc-random-number");
+submit.addEventListener ('click', function(){
 
+    // reset valori ad ogni click
+    user.innerHTML = "";
+    pcN.innerHTML = "";    
+    
+    let userValue = Math.floor(Math.random() * 5) +1;
+    let pcNValue = Math.floor(Math.random() * 5) +1;
+    console.log(userValue, pcNValue);
+    
+    
+    user.innerHTML = `Il tuo numero è: ${userValue}`;
+    pcN.innerHTML = `Il numero del PC è: ${pcNValue}`;
+
+    // ad ogni controllo resetto le classi in maniera opportuna
+    if (userValue > pcNValue){
+        pcWin.classList.add('hidden');
+        parity.classList.add('hidden');
+        userWin.classList.remove('hidden');        
+    }else if (userValue === pcNValue) {
+        pcWin.classList.add('hidden');
+        parity.classList.remove('hidden');
+        userWin.classList.add('hidden'); 
+    }else {
+        pcWin.classList.remove('hidden');
+        parity.classList.add('hidden');
+        userWin.classList.add('hidden'); 
+    }
+
+    
+    
+} )
